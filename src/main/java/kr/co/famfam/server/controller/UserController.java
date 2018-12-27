b70aa545adef81eb55743224dd45947ea755725d
Jaeyu Jo <ehay@naver.com>
2018-12-27 10:16:54 +0900

    Add UserController

---
+++ src/main/java/kr/co/famfam/server/controller/UserController.java
@@ -0,0 +1,10 @@
+package kr.co.famfam.server.controller;
+
+/**
+ * Created by ehay@naver.com on 2018-12-27
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class UserController {
+}
