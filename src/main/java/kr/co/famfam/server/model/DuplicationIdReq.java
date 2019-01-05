2903631e94af10bcc6572c36ddac079d7f726039
Jaeyu Jo <ehay@naver.com>
2019-01-05 11:33:40 +0900

    Fix UserService

---
+++ src/main/java/kr/co/famfam/server/model/DuplicationIdReq.java
@@ -0,0 +1,17 @@
+package kr.co.famfam.server.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by ehay@naver.com on 2019-01-05
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+@Data
+@NoArgsConstructor
+public class DuplicationIdReq {
+
+    private String userId;
+}
