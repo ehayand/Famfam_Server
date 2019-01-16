96ed53a2f3e016c6a210c8bc59fda6db8812af45
Jaeyu Jo <ehay@naver.com>
2019-01-16 13:18:39 +0900

    Update Content Response Body(add feel)

--- src/main/java/kr/co/famfam/server/service/FeelService.java
+++ src/main/java/kr/co/famfam/server/service/FeelService.java
@@ -2,6 +2,7 @@ package kr.co.famfam.server.service;
 
 import kr.co.famfam.server.model.DefaultRes;
 import kr.co.famfam.server.model.FeelReq;
+import kr.co.famfam.server.model.FeelRes;
 
 /**
  * Created by ehay@naver.com on 2018-12-25
@@ -12,6 +13,8 @@ import kr.co.famfam.server.model.FeelReq;
 public interface FeelService {
 
     DefaultRes findFeelsByContentIdx(int contentIdx);
+
+    public FeelRes findFeelsByContentIdxInternal(int contentIdx);
 
     DefaultRes countThisWeek(int userIdx);
 
